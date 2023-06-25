e5fc30de2c1eb00f7b0e9cde80d83a94f8dce66a
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-06-25 23:15:24 +0630

    improve

--- Laravel Filament.py
+++ Laravel Filament.py
@@ -21,7 +21,11 @@ class LaravelFilamentCommand(sublime_plugin.WindowCommand):
             if os.path.isfile("%s" % artisan_path):
                 self.command = kwargs.get('command', None)
                 self.fill_in_accept = kwargs.get('fill_in', False)
-                self.fill_in_label = kwargs.get('fill_in_lable', 'Enter the resource name')
+                self.fill_in_label = kwargs.get('fill_in_label', 'Enter the resource name')
+                self.fill_in_two_accept = kwargs.get('fill_in_two', False)
+                self.fill_in_two_label = kwargs.get('fill_in_two_label', 'Enter the resource name')
+                self.fill_in_three_accept = kwargs.get('fill_in_three', False)
+                self.fill_in_three_label = kwargs.get('fill_in_three_label', 'Enter the resource name')
                 self.args = [self.php_path, artisan_path]
                 if self.command is None:
                     self.window.show_input_panel('Command name w/o args:', '', self.on_command_custom, None, None)
@@ -41,8 +45,47 @@ class LaravelFilamentCommand(sublime_plugin.WindowCommand):
             self.on_done()
 
     def on_fill_in(self, fill_in):
-        self.args.extend(shlex.split(str(fill_in)))
-        self.on_done()
+        if 'make:filament-user' in self.command:
+            self.args.append('--name=' + fill_in)
+        else:
+            self.args.append(fill_in)
+
+        if self.fill_in_two_accept is True:
+            self.window.show_input_panel(self.fill_in_two_label, "", self.on_fill_in_two, None, None)
+        else:
+            self.on_done()
+
+    def on_fill_in_two(self, fill_in):
+        if fill_in != '':
+            if 'make:filament-user' in self.command:
+                self.args.append('--email=' + fill_in)
+            elif 'make:filament-page' in self.command:
+                self.args.append('--resource=' + fill_in)
+            elif 'make:filament-widget' in self.command:
+                self.args.append('--resource=' + fill_in)
+            else:
+                self.args.append(fill_in)
+
+            if self.fill_in_three_accept is True:
+                self.window.show_input_panel(self.fill_in_three_label, "", self.on_fill_in_three, None, None)
+            else:
+                self.on_done()
+        else:
+            self.on_done()
+
+    def on_fill_in_three(self, fill_in):
+        if fill_in != '':
+            if 'make:filament-user' in self.command:
+                self.args.append('--password=' + fill_in)
+            elif 'make:filament-page' in self.command:
+                self.args.append('--type=' + fill_in)
+            else:
+                self.args.append(fill_in)
+
+            self.on_done()
+            sublime.status_message(self.args)
+        else:
+            self.on_done()
 
     def on_command_custom(self, command):
         self.args.extend(shlex.split(str(command)))
