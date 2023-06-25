84de7341b1372b5dc340962c16257ddef3f98853
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-06-25 20:56:25 +0630

    initial commit

---
+++ Laravel Filament.py
@@ -0,0 +1,60 @@
+import os
+import shlex
+import subprocess
+import sublime
+import sublime_plugin
+
+class LaravelFilamentCommand(sublime_plugin.WindowCommand):
+    def __init__(self, *args, **kwargs):
+        super(LaravelFilamentCommand, self).__init__(*args, **kwargs)
+        settings = sublime.load_settings('Laravel Filament.sublime-settings')
+        self.php_path = settings.get('php_path')
+        self.artisan_path = settings.get('artisan_path')
+
+    def run(self, *args, **kwargs):
+        try:
+            # The first folder needs to be the Laravel Project
+            self.PROJECT_PATH = self.window.folders()[0]
+            artisan_path = os.path.join(self.PROJECT_PATH, self.artisan_path)
+            self.args = [self.php_path, artisan_path]
+
+            if os.path.isfile("%s" % artisan_path):
+                self.command = kwargs.get('command', None)
+                self.fill_in_accept = kwargs.get('fill_in', False)
+                self.fill_in_label = kwargs.get('fill_in_lable', 'Enter the resource name')
+                self.args = [self.php_path, artisan_path]
+                if self.command is None:
+                    self.window.show_input_panel('Command name w/o args:', '', self.on_command_custom, None, None)
+                else:
+                    self.on_command(self.command)
+            else:
+                sublime.status_message("Artisan not found")
+        except IndexError:
+            sublime.status_message("Please open a Laravel Project")
+
+    def on_command(self, command):
+        self.args.extend(shlex.split(str(self.command)))
+
+        if self.fill_in_accept is True:
+            self.window.show_input_panel(self.fill_in_label, "", self.on_fill_in, None, None)
+        else:
+            self.on_done()
+
+    def on_fill_in(self, fill_in):
+        self.args.extend(shlex.split(str(fill_in)))
+        self.on_done()
+
+    def on_command_custom(self, command):
+        self.args.extend(shlex.split(str(command)))
+        self.on_done()
+
+    def on_done(self):
+        if os.name != 'posix':
+            self.args = subprocess.list2cmdline(self.args)
+        try:
+            self.window.run_command("exec", {
+                "cmd": self.args,
+                "shell": os.name == 'nt',
+                "working_dir": self.PROJECT_PATH})
+        except IOError:
+            sublime.status_message('IOError - command aborted')
---
+++ README.md
@@ -0,0 +1,22 @@
+# Laravel Filament For Sublime Text
+
+A sublime text package for [Laravel Filament](https://filamentphp.com).
+
+## Installation
+
+- Open `Command Palette`
+- Select `Package Control: Add Repository`
+- Enter repository url `https://github.com/PyaeSoneAungRgn/sublime-laravel-filament`
+- Select `Package Control: Install Package`
+- Search `Laravel Filament`
+
+## Usage
+
+- Open `Command Palette`
+- Search `Laravel Filament`
+
+Please Check [Laravel Pint Official Documentation](https://laravel.com/docs/9.x/pint) for more information.
+
+## Credit
+
+[Laravel-5-Artisan](https://github.com/dydx/Laravel-5-Artisan)
